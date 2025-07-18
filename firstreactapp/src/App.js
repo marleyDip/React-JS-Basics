245b9bb8d01af359d3eb12c253547961ee50e9c8
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-07-19 04:38:26 +0600

    React Functional Component

--- firstreactapp/src/App.js
+++ firstreactapp/src/App.js
@@ -1,38 +1,29 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
 
 function App() {
-  let name = "Welcome to Z-Index Software company";
-  let num = [10, 20, 30, 40];
-
-  let obj = {
-    name: "Deep Akand",
-    cName: "MERN",
-    desc: "REACT JS",
-  };
-
-  let status = true;
   return (
     <div className="main">
-      <h1>{name}</h1>
+      <Header />
 
-      {num.map((num) => {
-        return <h4>{num}</h4>;
-      })}
+      <div className="row">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
 
-      <h2>{obj.name}</h2>
-      <h3>{obj.cName}</h3>
-      <h4>{obj.desc}</h4>
-
-      {status ? (
-        <h1 style={{ color: "red", backgroundColor: "yellow" }}>
-          Welcome to Z-Index Software company
-        </h1>
-      ) : (
-        ""
-      )}
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+function Card() {
+  return <div className="cardItems">Card div</div>;
+}
---
+++ firstreactapp/src/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div>
+      <h1>Welcome to Header Section</h1>
+      <h3>I Love You</h3>
+      <h4>I Love You Too</h4>
+    </div>
+  );
+}
+
+export default Header;
