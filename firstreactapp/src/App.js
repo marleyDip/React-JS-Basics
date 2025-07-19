5f3bcab0c8b0f6be817a47ede7306b6636e271c4
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-07-19 19:09:19 +0600

    Reactive Bootstrap

--- firstreactapp/src/App.js
+++ firstreactapp/src/App.js
@@ -2,28 +2,76 @@ import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
 
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+
 function App() {
   return (
     <div className="main">
-      <Header />
+      <Container fluid>
+        <Container>
+          <Row>
+            <Col className="col-12 text-center py-4">
+              <h1>Our Courses</h1>
+            </Col>
+          </Row>
 
-      <div className="row">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
+          <Row className="gy-4">
+            <Col lg="3" md="6">
+              <Card style={{ width: "18rem" }}>
+                <Card.Body className="border border-2 border-danger">
+                  <Card.Title>React</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
 
-      <Footer />
+            <Col lg="3" md="6">
+              <Card style={{ width: "18rem" }}>
+                <Card.Body className="border border-2 border-dark">
+                  <Card.Title>Next</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col lg="3" md="6">
+              <Card style={{ width: "18rem" }}>
+                <Card.Body className="border border-2 border-primary">
+                  <Card.Title>Node</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col lg="3" md="6">
+              <Card style={{ width: "18rem" }}>
+                <Card.Body className="border border-2 border-success">
+                  <Card.Title>MongoDB</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
     </div>
   );
 }
 
 export default App;
-
-function Card() {
-  return <div className="cardItems">Card div</div>;
-}
