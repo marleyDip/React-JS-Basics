dae11a3bd388430a9da604ebce497a5bc478bfaf
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-07-20 22:21:35 +0600

    Event Handling

--- firstweb/src/App.js
+++ firstweb/src/App.js
@@ -3,10 +3,46 @@ import Header from "./Header";
 import webLogo from "./images/logo.png";
 
 function App() {
+  let displayData = () => {
+    alert("Welcome to Deep");
+  };
+
+  let addData = (a, b) => {
+    console.log(a + b);
+  };
   return (
     <div className="App">
-      {/* <img src="https://marleydip.netlify.app/logo.png" alt="" />
-      <Header /> */}
+      <button
+        className="bg-[green] p-[10px] mr-4 text-white"
+        onClick={() => addData(10, 20)}
+      >
+        Add Data
+      </button>
+
+      {/* (10, 20) = automatic add 30 on console without click Add Data
+      
+      <button
+        className="bg-[green] p-[10px] mr-4 text-white"
+        onClick={addData(10, 20)} addData
+      >
+        Add Data
+      </button> */}
+
+      <button
+        className="bg-[red] p-[10px] my-10 text-white"
+        onClick={displayData}
+      >
+        Save
+      </button>
+
+      {/* displayData() = automatic fired without click on Save button 
+      
+      <button
+        className="bg-[red] p-[10px] my-10 text-white"
+        onClick={displayData()} 
+      >
+        Save
+      </button> */}
 
       <img width={200} src={webLogo} alt="" />
 
