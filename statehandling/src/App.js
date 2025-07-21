f31e772ca08306ee2f92c341fc7e4d840be34120
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-07-21 17:43:19 +0600

    Show & Hide Password Project

--- statehandling/src/App.js
+++ statehandling/src/App.js
@@ -6,23 +6,28 @@ import btnModule from "./Button.module.css";
 
 function App() {
   let [status, setStatus] = useState(false);
+  let [pStatus, setPStatus] = useState(false);
 
   let statusData = () => {
-    setStatus(!status);
+    setPStatus(!pStatus);
   };
 
   return (
     <div className="App">
+      <input className="input" type={pStatus ? "text" : "password"} />
+      <button className="button" onClick={statusData}>
+        {pStatus ? "Hide" : "Show"}
+      </button>
+      <br />
+      <br />
       <button className={btnModule.error}>Error</button>
       <button className={btnModule.warning}>Demo</button>
       {/* <button className="button" onClick={statusData}>
         Show
       </button> */}
-
       <button className="button" onClick={() => setStatus(!status)}>
         {status ? "Hide" : "Show"}
       </button>
-
       {status ? (
         <p className="para">Welcome to Z-Index Next Gen Stack Company</p>
       ) : (
