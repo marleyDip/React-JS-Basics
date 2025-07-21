85d78ca2e930c9476122db696f9b34857fde43fc
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-07-21 19:08:38 +0600

    Responsive Menu | Project

--- statehandling/src/App.js
+++ statehandling/src/App.js
@@ -1,38 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
 import btnModule from "./Button.module.css";
 
 function App() {
-  let [status, setStatus] = useState(false);
-  let [pStatus, setPStatus] = useState(false);
-
-  let statusData = () => {
-    setPStatus(!pStatus);
-  };
+  let [menuStatus, setMenuStatus] = useState(false);
 
   return (
     <div className="App">
-      <input className="input" type={pStatus ? "text" : "password"} />
-      <button className="button" onClick={statusData}>
-        {pStatus ? "Hide" : "Show"}
+      <button className="mIcon" onClick={() => setMenuStatus(!menuStatus)}>
+        &#9776;
       </button>
-      <br />
-      <br />
-      <button className={btnModule.error}>Error</button>
-      <button className={btnModule.warning}>Demo</button>
-      {/* <button className="button" onClick={statusData}>
-        Show
-      </button> */}
-      <button className="button" onClick={() => setStatus(!status)}>
-        {status ? "Hide" : "Show"}
-      </button>
-      {status ? (
-        <p className="para">Welcome to Z-Index Next Gen Stack Company</p>
-      ) : (
-        ""
-      )}
+
+      <div className={`menu ${menuStatus ? "activeMenu" : ""}`}>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Course</li>
+          <li>Gallery</li>
+          <li>Contact</li>
+        </ul>
+      </div>
     </div>
   );
 }
