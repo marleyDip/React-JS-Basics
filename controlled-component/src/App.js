29c601a8a9004fe6484d3779135b1d95e75a0113
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-07-23 01:37:37 +0600

    Use React Controlled Components Input

--- controlled-component/src/App.js
+++ controlled-component/src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [uname, setUname] = useState("");
+  let [password, setPassword] = useState("");
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(uname, password);
+  };
+
+  // let getUname = (event) => {
+  // console.log("hello");
+  //   setUname(event.target.value);
+  // };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <form onSubmit={handleSubmit}>
+              <div className="text-start my-3">
+                <label>UserName</label>
+
+                <input
+                  type="text"
+                  value={uname}
+                  //onChange={getUname}
+                  onChange={(event) => setUname(event.target.value)}
+                  className="form-control"
+                />
+              </div>
+
+              <div className="text-start my-3">
+                <label>Password</label>
+
+                <input
+                  type="text"
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                  className="form-control"
+                />
+              </div>
+
+              <div className="text-start my-3">
+                <button>Login</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
